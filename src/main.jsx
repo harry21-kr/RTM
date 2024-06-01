e4aae2495733d2546aae0742b0276d418aaa2f09
박원빈 <harry21@kakao.com>
2024-06-01 12:51:35 +0900

    🎉 add : redux structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import Router from './routes/Router';
+import store from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </React.StrictMode>
 );
