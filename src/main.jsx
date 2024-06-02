6667a61d5b44365a563ca924008fb6f4c0c5d18d
박원빈 <harry21@kakao.com>
2024-06-03 01:59:39 +0900

    🎉 add : GlobalStyles

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import GlobalStyles from './components/GlobalStyles';
 import Router from './routes/Router';
 import store from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyles />
       <Router />
     </Provider>
   </React.StrictMode>
