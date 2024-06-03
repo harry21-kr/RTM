fcbaceb4abeae3aaef8b8f48ca083afc97b9edc9
Park Won Bin <119783002+harry21-kr@users.noreply.github.com>
2024-06-03 17:29:14 +0900

    로그인 페이지 관련 PR입니다. (#1)
    * 🎉 add : AuthContext, Providers component
    
    * 🎉 add : LoginForm layout
    
    * ♻️ refactor : AuthContext: delete try/catch
    
    * 🎉 add : navigate when isLoggedIn
    
    * 🎉 add : auth page layout, logic

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authSlice from './slices/auth/auth.slice';
 import postSlice from './slices/post/post.slice';
 
 const store = configureStore({
-  reducer: { auth: authSlice, post: postSlice }
+  reducer: { post: postSlice }
 });
 
 export default store;
