b2c78516b52d8a495d3f7b9938a44dbafd896436
Park Won Bin <119783002+harry21-kr@users.noreply.github.com>
2024-06-04 22:39:13 +0900

    Modal Context 관련 PR입니다. #2

--- src/Providers.jsx
+++ src/Providers.jsx
@@ -1,11 +1,14 @@
 import { Provider as ReduxProvider } from 'react-redux';
 import AuthProvider from './contexts/Auth/AuthContext';
+import ModalProvider from './contexts/Modal/ModalContext';
 import store from './store/store';
 
 export default function Providers({ children }) {
   return (
     <ReduxProvider store={store}>
-      <AuthProvider>{children}</AuthProvider>
+      <AuthProvider>
+        <ModalProvider>{children}</ModalProvider>
+      </AuthProvider>
     </ReduxProvider>
   );
 }
