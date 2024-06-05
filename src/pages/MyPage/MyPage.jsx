8a62f5680909eb59d9951fec35c19f0a2cc41206
Park Won Bin <119783002+harry21-kr@users.noreply.github.com>
2024-06-06 05:22:15 +0900

    포스트 모달 관련 PR입니다. (#10)
    * ♻️ refactor : useModal hook
    
    * 🎉 add : PostModal component
    
    * 🩹 fix : addPosts logic

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,10 +1,9 @@
-import MyPageHeader from '../../components/Header/MyPage';
-import React from 'react';
-import Profile from '../../components/MyPage/Profile';
-import Modify from '../../components/MyPage/Modify';
 import styled from 'styled-components';
-import { PostingList } from '../../components/MyPage/PostingList';
+import MyPageHeader from '../../components/Header/MyPage';
 import { Calendar } from '../../components/MyPage/Calendar';
+import Modify from '../../components/MyPage/Modify';
+import { PostingList } from '../../components/MyPage/PostingList';
+import Profile from '../../components/MyPage/Profile';
 
 export default function MyPage() {
   return (
