ada8200e913f6804b1870440a6cded50c4dbead3
Park Won Bin <119783002+harry21-kr@users.noreply.github.com>
2024-06-07 00:38:23 +0900

    Revert "Modal Context 관련 PR입니다. #2"
    This reverts commit b2c78516b52d8a495d3f7b9938a44dbafd896436.

--- src/Providers.jsx
+++ src/Providers.jsx
@@ -1,14 +1,11 @@
 import { Provider as ReduxProvider } from 'react-redux';
 import AuthProvider from './contexts/Auth/AuthContext';
-import ModalProvider from './contexts/Modal/ModalContext';
 import store from './store/store';
 
 export default function Providers({ children }) {
   return (
     <ReduxProvider store={store}>
-      <AuthProvider>
-        <ModalProvider>{children}</ModalProvider>
-      </AuthProvider>
+      <AuthProvider>{children}</AuthProvider>
     </ReduxProvider>
   );
 }
