bfdf2df5a0365d95c92e01e51877df163c23ff1a
Park Won Bin <119783002+harry21-kr@users.noreply.github.com>
2024-06-07 00:38:23 +0900

    Revert "🎉 add : GlobalStyles"
    This reverts commit ea469bb44b2c6603cab8330d2bd16cc1033ecc6d.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import GlobalStyles from './components/GlobalStyles';
 import Router from './routes/Router';
 import store from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <GlobalStyles />
       <Router />
     </Provider>
   </React.StrictMode>
