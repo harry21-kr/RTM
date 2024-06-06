b4b00edf88f677711d22427870fb08f2437c7ee7
Park Won Bin <119783002+harry21-kr@users.noreply.github.com>
2024-06-06 17:50:48 +0900

    포스트 모달 버그 수정 관련 PR입니다. (#12)

--- src/components/MyPage/PostingList.jsx
+++ src/components/MyPage/PostingList.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { useModal } from '../../contexts/Modal/useModal';
-import PostModal from '../Modal/PostModal';
+import PostItem from './PostItem';
 import UseFetchPosts from './hooks/UseFetchPosts';
 
 export function PostingList() {
   const posts = useSelector((state) => state.posts.posts);
-  const [selectedPostData, setSelectedPostData] = useState({});
-  const [openModal] = useModal(<PostModal data={selectedPostData} />);
 
   UseFetchPosts(); // 커스텀 훅 호출
 
@@ -16,17 +12,7 @@ export function PostingList() {
     <>
       <StListWrap>
         {posts.map((post) => (
-          <StPostItem
-            key={post.id}
-            onClick={() => {
-              setSelectedPostData(post);
-              openModal();
-            }}
-          >
-            <h3>{post.title}</h3>
-            <p>{post.content}</p>
-            <img src={post.img_url} alt="post image"></img>
-          </StPostItem>
+          <PostItem key={post.id} post={post} />
         ))}
       </StListWrap>
     </>
@@ -39,33 +25,4 @@ const StListWrap = styled.div`
   gap: 20px;
   background-color: beige;
   padding: 20px;
-`;
-
-const StPostItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  background-color: white;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-
-  > h3 {
-    grid-area: title;
-    font-size: 1.3rem;
-  }
-
-  > p {
-    grid-area: content;
-  }
-
-  > img {
-    grid-area: image;
-    width: 100%;
-    height: auto;
-    max-height: 200px;
-    object-fit: cover;
-  }
 `;
