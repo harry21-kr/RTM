ef24e4bf64c86034375eeabed72a8cbe2a47588a
박원빈 <harry21@kakao.com>
2024-06-06 19:10:26 +0900

    ♻️ refactor : remove write route

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,16 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from '../pages/HomePage/HomePage';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { useAuth } from '../contexts/Auth/hooks';
 import LoginPage from '../pages/LoginPage/LoginPage';
-import MyPage from '../pages/MyPage/MyPage';
-import WritePage from '../pages/WritePage/WritePage';
 import ModifyPage from '../pages/ModifyPage/ModifyPage';
+import MyPage from '../pages/MyPage/MyPage';
 
 export default function Router() {
+  const { session } = useAuth();
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/write" element={<WritePage />} />
+        <Route path="/" element={<Navigate to={session ? '/:userName/posts' : '/login'} replace={true} />} />
         <Route path="/ModifyPage" element={<ModifyPage />} />
         <Route path="/:userName/posts" element={<MyPage />} />
         <Route path="/login" element={<LoginPage />} />
