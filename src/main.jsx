fc7f063e184e938b4434d03bcf5a6c0b9177704d
Park Won Bin <119783002+harry21-kr@users.noreply.github.com>
2024-06-07 00:38:23 +0900

    Revert "🎉 add : redux structure"
    This reverts commit 9f7d2faaa1df41d86c17eb4c9134b747c4425871.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import Router from './routes/Router';
-import store from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router />
-    </Provider>
+    <Router />
   </React.StrictMode>
 );
--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authSlice from './slices/auth/auth.slice';
-
-const store = configureStore({
-  reducer: { auth: authSlice }
-});
-
-export default store;
