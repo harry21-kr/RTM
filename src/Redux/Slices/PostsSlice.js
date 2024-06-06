57e8cde4f634ba0e766c4f23ccddcba687b9db79
Park Won Bin <119783002+harry21-kr@users.noreply.github.com>
2024-06-06 18:33:38 +0900

    포스트 수정, 삭제 상태 관리 로직 관련 PR입니다.

--- src/Redux/Slices/PostsSlice.js
+++ src/Redux/Slices/PostsSlice.js
@@ -8,14 +8,21 @@ const PostsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    addPosts: (state, action) => {
-      state.posts = [...state.posts, action.payload];
-    },
     initPosts: (state, action) => {
       state.posts = action.payload;
+    },
+    addPost: (state, action) => {
+      state.posts = [...state.posts, action.payload];
+    },
+    deletePost: (state, action) => {
+      state.posts = state.posts.filter(({ id }) => id !== action.payload.id);
+    },
+    editPost: (state, action) => {
+      const newPosts = state.posts.map((post) => (post.id === action.payload.id ? action.payload : post));
+      state.posts = newPosts;
     }
   }
 });
 
-export const { addPosts, initPosts } = PostsSlice.actions;
+export const { initPosts, addPost, deletePost, editPost } = PostsSlice.actions;
 export default PostsSlice.reducer;
