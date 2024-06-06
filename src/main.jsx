cb88c0d7797d98765329f87fb276d24c1fd72619
Park Won Bin <119783002+harry21-kr@users.noreply.github.com>
2024-06-07 00:38:23 +0900

    Revert "🎉 add : default project structure"
    This reverts commit 7050e40dc3aa6f385c51477c597c3d3d506915fc.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './routes/Router';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <App />
   </React.StrictMode>
 );
